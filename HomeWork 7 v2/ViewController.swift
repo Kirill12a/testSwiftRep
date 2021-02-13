e404e3c31487c813850b1c472a2f1e537bb2e920
Kirill12a <kirilldrozd12@gmail.com>
2021-02-14 00:17:01 +0300

    Initial Commit

---
+++ HomeWork 7 v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork 7 v2
+//
+//  Created by Kirill Drozdov on 14.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
